9c38ef9ac8b9c5a9c6893d835d9a4d3363af9ebc
Juan Ontiveros <juan.ontiveros@zerf.com.ar>
2021-08-18 23:43:22 -0300

    Agregando Organizacion

---
+++ src/main/java/negocio/Organizacion.java
@@ -0,0 +1,25 @@
+package negocio;
+
+public class Organizacion {
+    private static Organizacion instance = null;
+    private String nombre;
+
+    private Organizacion(){
+        this.nombre = null;
+    }
+
+    public static Organizacion getInstance(){
+        if(instance == null){
+            instance = new Organizacion();
+        }
+        return instance;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
