3f9bf89eee8a09482b08a76974acdc871a663dd5
Martu Mattioli <martinamattioli0@gmail.com>
2021-08-19 23:34:25 -0300

    adding getter/setter

--- src/main/java/negocio/Prestamo.java
+++ src/main/java/negocio/Prestamo.java
@@ -7,6 +7,7 @@ public class Prestamo {
     private List<Cuota> cuotas;
     private Componente componente;
     private Float tasaDeInteres;
+    private EstadoCuota estado;
 
     public Prestamo(Componente componente, Float tasaDeInteres) {
         this.componente = componente;
@@ -14,4 +15,35 @@ public class Prestamo {
         cuotas = new ArrayList<Cuota>();
     }
 
+    public List<Cuota> getCuotas() {
+        return cuotas;
+    }
+
+    public void setCuotas(List<Cuota> cuotas) {
+        this.cuotas = cuotas;
+    }
+
+    public Componente getComponente() {
+        return componente;
+    }
+
+    public void setComponente(Componente componente) {
+        this.componente = componente;
+    }
+
+    public Float getTasaDeInteres() {
+        return tasaDeInteres;
+    }
+
+    public void setTasaDeInteres(Float tasaDeInteres) {
+        this.tasaDeInteres = tasaDeInteres;
+    }
+
+    public EstadoCuota getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCuota estado) {
+        this.estado = estado;
+    }
 }
