9f97d105e9d577c26e49eef0d5aaca07aa84eb0d
Martu Mattioli <martinamattioli0@gmail.com>
2021-08-21 12:09:45 -0300

    changing method calcularCosto in class moto

--- src/main/java/negocio/envio/Moto.java
+++ src/main/java/negocio/envio/Moto.java
@@ -5,6 +5,7 @@ import negocio.componente.Componente;
 public class Moto implements TipoDeEnvio{
 
     public Integer calcularCosto(Componente componente) {
-        return 150;
+        if(componente.getPeso() <= 200) return 150;
+        else return (componente.getPeso()*2);
     }
 }
