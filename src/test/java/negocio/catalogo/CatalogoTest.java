b96d8a89d026118eb60c83def77cfa28317e9af8
Juan Ontiveros <juan.ontiveros@zerf.com.ar>
2021-08-21 19:56:59 -0300

    Agregando tests para Catalogo

---
+++ src/test/java/negocio/catalogo/CatalogoTest.java
@@ -0,0 +1,42 @@
+package negocio.catalogo;
+
+import negocio.componente.Foto;
+import negocio.componente.Producto;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CatalogoTest {
+
+    static Catalogo catalogo;
+    static Producto celular;
+    static Producto televisor;
+
+    @BeforeClass
+    public static void init(){
+        catalogo = new Catalogo();
+        televisor = new Producto("Lg 55 4k", "Televisor", 300,
+                    new Foto(), 10);
+        celular = new Producto("Samsung S21", "Celular de alta gama", 100,
+                    new Foto(), 4);
+        catalogo.agregar(celular);
+        catalogo.agregar(televisor);
+    }
+
+    @Test
+    public void testDadoUnNombreSeRealizaLaBusquedaCorrectamenteEnElCatalogo(){
+        Assert.assertEquals(celular, catalogo.buscarPorNombre("Samsung S21"));
+    }
+
+    @Test
+    public void testAlFiltrarDelCatalogoPorPrecioMayorOIgualObtengoUnaListaDeDosProducto(){
+        Assert.assertEquals(2, catalogo.filtrarComponentesConPrecioMayorOIgual(100).size());
+    }
+
+    @Test
+    public void testAlFiltrarDelCatalogoPorPrecioMenorOIgualObtengoUnaListaDeUnProducto(){
+        Assert.assertEquals(1, catalogo.filtrarComponentesConPrecioMenorOIgual(150).size());
+    }
+
+}
+
--- src/test/java/negocio/organizacion/OrganizacionTest.java
+++ src/test/java/negocio/organizacion/OrganizacionTest.java
@@ -15,18 +15,10 @@ import org.junit.Test;
 public class OrganizacionTest {
 
     static Organizacion organizacion;
-    static Producto celular;
-    static Producto televisor;
 
     @BeforeClass
     public static void init(){
         organizacion = Organizacion.getInstance();
-        televisor = new Producto("Lg 55 4k", "Televisor", 300,
-                new Foto(), 10);
-        celular = new Producto("Samsung S21", "Celular de alta gama", 100,
-                new Foto(), 4);
-        organizacion.agregarAlCatalogo(televisor);
-        organizacion.agregarAlCatalogo(celular);
     }
 
     @Test
@@ -42,26 +34,18 @@ public class OrganizacionTest {
     }
 
     @Test
+    public void testAgregarUnProductoLoAgregaAlCatalogo(){
+        Producto producto = new Producto("Producto Test", "Test", 200, new Foto(), 10);
+        organizacion.agregarAlCatalogo(producto);
+        Assert.assertEquals(1, organizacion.getCatalogo().obtenerTamaño().intValue());
+    }
+
+    @Test
     public void testRegistrarUnPedidoLoAgregaCorrectamenteALaOrganizacion(){
         Cliente cliente = new Cliente("Lionel Messi", "test@dds.com", TipoDeDocumento.DNI, "12345678", "Calle 123", null, Ubicacion.GRAN_BSAS);
         Componente componente = new Producto("Producto Test", "Test", 200, new Foto(), 5);
         TipoDeEnvio tipoDeEnvio = new Moto();
         organizacion.registrarPedido(cliente, componente, tipoDeEnvio);
         Assert.assertEquals(1, organizacion.getPedidos().size());
-    }
-
-    @Test
-    public void testDadoUnNombreSeRealizaLaBusquedaCorrectamenteEnElCatalogo(){
-        Assert.assertEquals(celular, organizacion.buscarEnElCatalogoPorNombre("Samsung S21"));
-    }
-
-    @Test
-    public void testAlFiltrarDelCatalogoPorPrecioMenorOIgualObtengoUnaListaDeUnProducto(){
-        Assert.assertEquals(1, organizacion.filtrarDelCatalogoPorPrecioMenorOIgual(150).size());
-    }
-
-    @Test
-    public void testAlFiltrarDelCatalogoPorPrecioMayorOIgualObtengoUnaListaDeDosProducto(){
-        Assert.assertEquals(2, organizacion.filtrarDelCatalogoPorPrecioMayorOIgual(100).size());
     }
 }
